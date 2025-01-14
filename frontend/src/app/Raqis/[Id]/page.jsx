c4f5b388d142d2d1a6408cd2c65d24ecd3e91d7c
Farhad Lafarie <farhadlafarie@gmail.com>
2025-01-14 21:24:37 +0530

    refactor: simplify Raqis component by replacing useRouter with useParams and removing unused state and effects

--- frontend/src/app/Raqis/[Id]/page.jsx
+++ frontend/src/app/Raqis/[Id]/page.jsx
@@ -1,35 +1,15 @@
 'use client'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { Router } from 'next/navigation';
+import { useParams } from 'next/navigation';
 
 function Raqis (){
-  const router = useRouter();
-  const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   if (router.isReady) {
-  //     const { Id } = router.query;
-  //     if (Id) {
-  //       fetch(`/api/data/${Id}`)
-  //         .then(response => response.json())
-  //         .then(data => setData(data))
-  //         .catch(error => console.error('Error fetching data:', error));
-  //     }
-  //   }
-  // }, [router.isReady]);
-
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+  const params = useParams();
+  const Id = params.Id;
 
   return (
-    <div>
-      <h1>Data for ID: {router.query.Id}</h1>
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    <div className='mb-56 text-black'>
+      <h1>Data for ID: {Id}</h1>
     </div>
   );
 };
-
 
 export default Raqis;
