c123e9981d1c5b902ea9fb9201020e4d4af9db6d
aathiq <aathiq.20210466@iit.ac.lk>
2025-02-05 00:27:47 +0530

    get today meeting access to admin

--- api-service/src/routes/meetingRoutes.ts
+++ api-service/src/routes/meetingRoutes.ts
@@ -10,7 +10,7 @@ import {authorizeRoles, protect} from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.get('/get-meetings', protect, authorizeRoles('super-admin'), getAllMeetings);
-router.get('/get-today-meetings', protect, authorizeRoles('super-admin'), getTodayAndFutureMeetings);
+router.get('/get-today-meetings', protect, authorizeRoles('super-admin',"admin"), getTodayAndFutureMeetings);
 router.post('/add-meetings', protect, authorizeRoles('admin', 'user'), addMeeting);
 router.get('/get-meetings/user/', protect, authorizeRoles('user'), getMeetingsByUserId);
 router.get('/get-meetings/raki/', protect, authorizeRoles('admin', 'super-admin'), getMeetingsByRakiId);
