bcd98eb745efbe234b3853c546d544d9301f6285
aathiq <aathiq.20210466@iit.ac.lk>
2025-02-17 13:32:20 +0530

    duplicate removed

--- api-service/src/app.ts
+++ api-service/src/app.ts
@@ -77,12 +77,12 @@ app.post(
     })
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://propheticruqyah.com");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://propheticruqyah.com");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
 
 app.use(cors(corsOptions));
 app.use(express.json());
