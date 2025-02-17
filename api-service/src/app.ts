5daa069b1a45dfde992990afad746874d5665157
aathiq <aathiq.20210466@iit.ac.lk>
2025-02-17 11:07:13 +0530

    cors error

--- api-service/src/app.ts
+++ api-service/src/app.ts
@@ -76,6 +76,14 @@ app.post(
 
     })
 
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://propheticruqyah.com");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
