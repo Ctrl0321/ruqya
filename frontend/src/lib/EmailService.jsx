c810ca0f547bd80b0120fadcd0ff04b721add30d
aathiq <aathiq.20210466@iit.ac.lk>
2025-02-22 03:33:26 +0530

    forgot password added to client side

--- frontend/src/lib/EmailService.jsx
+++ frontend/src/lib/EmailService.jsx
@@ -1,5 +1,8 @@
 import emailjs from "@emailjs/browser";
 
+const resetHeading="We received a request to reset the password for your Ruqya website. To proceed with resetting your password, please use the following 6-digit verification code:"
+
+
 export const sendEmail = async (templateID, params) => {
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "";
     const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "";
@@ -26,6 +29,17 @@ export const sendEmail = async (templateID, params) => {
         console.error("EmailJS Error:", error);
     }
     return false;
+};
+
+export const sendOtpEmail = async (user_email, otp,heading=resetHeading,name) => {
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_OTP;
+
+    if (!templateID) {
+        console.error("Email template ID is missing.");
+        return false;
+    }
+
+    return sendEmail(templateID, {user_email, name, otp,heading })
 };
 
 export const sendMeetingEmail = async (
